5d244ee06bc5ce6c29d168adc950ccdff1659c7c
id774 <idnanashi@gmail.com>
2014-11-05 17:49:58 +0900

    9 Generic Data Clips

--- public/js/d3js.data-clips.js
+++ public/js/d3js.data-clips.js
@@ -1,13 +1,12 @@
-var format = d3.time.format("%Y-%m-%d"),
-  margin = {top: 20, right: 20, bottom: 30, left: 40},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
- 
+var margin = {top: 20, right: 20, bottom: 30, left: 40};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
 var force = d3.layout.force()
   .gravity(0.1)
-  .charge(function(d, i) { return d.amount * -1; })
+  .charge(function(d, i) { return d.score * -1; })
   .size([width, height]);
-   
+
 var color = d3.scale.category10();
 var svg = d3.select("body")
   .append("svg")
@@ -19,9 +18,9 @@ var svg = d3.select("body")
   d3.json('../json/data-clips.json', function(error, data) {
     console.log(data)
     data.values.forEach(function(d) {
-      d.date = format.parse(String(d[0]).substring(0, 10));
-      d.amount = d[1];
-      console.log("Spent $" + d.amount + " on " + d.date);
+      d.word = String(d[0]);
+      d.score = d[1];
+      console.log("Score " + d.score + " on " + d.word);
     });
   force
     .nodes(data.values)
@@ -34,31 +33,32 @@ var svg = d3.select("body")
     .attr("class", "node")
     .call(force.drag);
   node.append("circle")
-    .attr("r", function(d) { return d.amount * .1; })
+    .attr("r", function(d) { return d.score * .1; })
     .attr("opacity", .67)
     .attr("fill", function(d){
-      if (d.amount <= 800) {
+      if (d.score <= 800) {
         return "#27ae60" // Green
-      } else if (d.amount > 800 && d.amount <= 900) {
+      } else if (d.score > 800 && d.score <= 900) {
         return "#f1c40f" // Yellow
-      } else if (d.amount > 900 && d.amount <= 1000) {
+      } else if (d.score > 900 && d.score <= 1000) {
         return "#e67e22" // Orange
       }
     });
   node.append("text")
-    .text(function(d){ return d.date.toString().substring(4,7); })
+    .text(function(d){ return d.word; })
     .attr('fill', '#fff')
     .attr('font-size', 24)
     .attr('dx', -16)
     .attr('dy', -5);
   node.append("text")
-    .text(function(d){ return '$' + d.amount; })
+    .text(function(d){ return d.score; })
     .attr('fill', '#fff')
     .attr('dx', -25)
     .attr('dy', 15);
   force.on("tick", function() {
     node
-    .attr('transform', function(d) { return 'translate('+ Math.max(20, Math.min(width-20, d.x)) + ',' + Math.max(20, Math.min(height-20, d.y)) + ')';     });
+    .attr('transform', function(d) {
+      return 'translate('+ Math.max(20, Math.min(width-20, d.x)) + ','
+        + '' + Math.max(20, Math.min(height-20, d.y)) + ')'; });
   });
 })
-
